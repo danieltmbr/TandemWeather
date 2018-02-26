37ddecc86005c4dbde8e5783f10082c847a587de
Daniel Tombor <t.dani12@gmail.com>
2018-02-26 23:31:56 +0100

    Routing UI refinements

--- TandemForecast/Routers/AppRouter.swift
+++ TandemForecast/Routers/AppRouter.swift
@@ -8,36 +8,45 @@
 
 import UIKit
 
-
 final class AppRouter {
+
+    // MARK: - Properties
 
     private let window: UIWindow
 
     private let tabbarController: UITabBarController
 
+    // MARK: - Initialisation
+
     init(window: UIWindow) {
         self.window = window
         tabbarController = UITabBarController()
-
     }
 
-    func start() {
+    // MARK: - Public methods
 
+    func start() {
+        tabbarController.viewControllers = [
+            UINavigationController(rootViewController: onlineForecastScene()),
+            UINavigationController(rootViewController: offlineForecastScene())
+        ]
+        window.rootViewController = tabbarController
+        window.makeKeyAndVisible()
+    }
+
+    // MARK: - Private methods
+
+    private func onlineForecastScene() -> UIViewController {
         let onlineViewModel = ForecastListViewModel(service: ForecastFetcher(), cityName: "London")
         let onlineListViewController = ForecastListViewController(model: onlineViewModel)
         onlineListViewController.tabBarItem = UITabBarItem(title: "Open weather", image: #imageLiteral(resourceName: "apiTabBarIcon"), selectedImage: nil)
-        onlineListViewController.title = "London"
+        return onlineListViewController
+    }
 
+    private func offlineForecastScene() -> UIViewController {
         let onlineViewModel2 = ForecastListViewModel(service: ForecastFetcher(), cityName: "London")
         let csvListViewController = ForecastListViewController(model: onlineViewModel2)
         csvListViewController.tabBarItem = UITabBarItem(title: "Offline", image: #imageLiteral(resourceName: "csvTabBarIcon"), selectedImage: nil)
-
-        tabbarController.viewControllers = [
-            UINavigationController(rootViewController: onlineListViewController),
-            UINavigationController(rootViewController: csvListViewController)
-        ]
-
-        window.rootViewController = tabbarController
-        window.makeKeyAndVisible()
+        return csvListViewController
     }
 }
